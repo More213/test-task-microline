28c037bc917d85ea07d8de4af55148c63190b6a3
More213 <esenin.teen@gmail.com>
2022-11-24 00:22:05 +0300

    create app

--- src/DownloadFile/index.tsx
+++ src/DownloadFile/index.tsx
@@ -2,51 +2,44 @@ import { dividerClasses } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-
+import styles from './index.module.scss'
 
 const DownloadFileComponent = () => {
   const [value, setValue] = useState('')
   const [extension, setExtension] = useState('')
-  const rexIllicitFile = /\.(js|php|html|htmlx|css|)$/gm;
+  const rexIllicitFile = /\.(js|php|html|htmlx|css)$/gm;
   const rexFirstLvl = /^(http|https)\:\/\/[^\/]+/gm
 
 
-  useEffect(() => {
-    console.log(!rexIllicitFile.test(value))
-    if(!rexIllicitFile.test(value)) {
-      console.log(!rexFirstLvl.test(value))
-    }
-  }, [value])
-
   const onDownloadFile = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("content-type",  'application/x-www-form-urlencoded');
+    const myRequest = new Request(value, {
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'cors',
+      cache: 'default',
+    });
 
+    fetch(myRequest)
+    .then((res) => {
+      console.log(res.headers.get('content-type'))
+      return res.blob()
+    })
+    .then((blob) => {
+       const link = document.createElement("a");
+                link.download = 'filename';
 
-//     fetch( value, { 
-//       method: 'get', 
-//       mode: 'no-cors', 
-//       referrerPolicy: 'no-referrer',
+          link.href = URL.createObjectURL(blob);
+    console.log(link)
+          document.body.appendChild(link);
+          // link.download = 'filename';
+          link.click();
 
-//     },
-//     )
-//     .then(response => {
-//       console.log(response)
-//       return response.blob()
-//     })
-//     .then(res => {
-//       const link = document.createElement("a");
-//             link.download = 'filename';
-
-//       link.href = URL.createObjectURL(res);
-// console.log(link)
-//       // document.body.appendChild(link);
-//       // link.download = 'filename';
-//       link.click();
-
-//       // URL.revokeObjectURL(link.href);
-//       // document.body.removeChild(link);
-//   })
-//   .catch(console.error);
-//   console.log('файл не найден')
+      URL.revokeObjectURL(link.href);
+      document.body.removeChild(link);
+    })
+    .catch(console.error);
   }
 
   const onChangeValue = (val: string) => {
@@ -54,7 +47,7 @@ const DownloadFileComponent = () => {
   }
 
   return (
-    <div>
+    <div className={styles.txtFieldWrap}>
       <TextField id="standard-basic"
         defaultValue={value}
         label="Standard"
